b3dbeb03d2c602bd362a416d1c6265a5d2958bb9
Ilya Dolgorukov <web@difabrik.ru>
2020-10-19 17:05:58 +0500

    homework 6

---
+++ homework6/03.py
@@ -0,0 +1,26 @@
+# Реализовать базовый класс Worker (работник), в котором определить атрибуты: name, surname,
+# position (должность), income (доход). Последний атрибут должен быть защищенным и ссылаться на словарь,
+# содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+# Создать класс Position (должность) на базе класса Worker. В классе Position реализовать методы получения
+# полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self.income = {'wage': wage, 'bonus': bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return self.income['wage'] + self.income['bonus']
+
+
+man = Position('Илья', 'Долгоруков', 'Разработчик', 150000, 80000)
+print(f'{man.position}: {man.get_full_name()}')
+print(f'Доход: {man.get_total_income()} руб. в месяц')
